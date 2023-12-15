1b81e09827e14e30e8127d5f4da88974ac639531
lmsandovalm <lmsandoval367@misena.edu.co>
2023-12-14 22:49:16 -0500

    ProyectoAprendix

---
+++ app/src/main/java/com/laurasando/mobile_aprendix/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.laurasando.mobile_aprendix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
